66cd5ee25a636ba274750c243061102ee97ceae2
aleksandr.pogozhii <aleksandr.pogozhii@devexpress.com>
2023-11-08 23:29:53 +0000

    font

--- src/components/checkout-form.jsx
+++ src/components/checkout-form.jsx
@@ -5,9 +5,9 @@ const CheckoutForm = ({basket}) => {
 
     return (    
         <form className="cart-form">
-            <h3 className="cart-form__title text__size_lbb"> Оформление заказа</h3>
+            <h3 className="cart-form__title"> Оформление заказа</h3>
             <input onChange={dummy} className="cart-form__input text-input text__size_s" type="text" name="" id="" placeholder="Имя Фамилия"></input>
-            <input onChange={dummy} className="cart-form__input text-input text__size_s" type="tel" name="" id="" placeholder="Номер телефона"></input>
+            <input onChange={dummy} className="cart-form__input text-input text__size_s" type="tel" name="" id="" placeholder="+ 7 904 000 80 80"></input>
             <input onChange={dummy} className="cart-form__input text-input text__size_s" type="text" name="" id="" placeholder="Адрес Доставки"></input>
             <div className="cart-form__price-box">
                 <span className="text__size_l">Итого: </span>
