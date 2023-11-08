f2684b616fd96d2ea920f2843bf6b5e230c96941
aleksandr.pogozhii <aleksandr.pogozhii@devexpress.com>
2023-11-08 18:53:37 +0000

    class name

--- src/components/product-cart.jsx
+++ src/components/product-cart.jsx
@@ -14,8 +14,8 @@ const ProductCart = ({product, basket}) => {
                 <img className="product__image" src={"img/product/" + product.imageUrl} alt={product.name}/>
             </div>
             <h3 className="product__title text__size_lb">{product.name}</h3>
-            <p className="product__desc text__size_s text_color_gray">{product.description}</p>
-            <p className="text__size_lb text_color_quite-dark">{product.price} руб.</p>
+            <p className="product__desc text__size_s text__color_gray">{product.description}</p>
+            <p className="text__size_lb text__color_quite-dark">{product.price} руб.</p>
     
         </div>
     );
