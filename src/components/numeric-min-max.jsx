96e2bdd1bf9fcf9007db2bbc900085c9066ba82f
aleksandr.pogozhii <aleksandr.pogozhii@devexpress.com>
2023-11-08 18:48:50 +0000

    svg react errors

--- src/components/numeric-min-max.jsx
+++ src/components/numeric-min-max.jsx
@@ -28,12 +28,12 @@ const NumericMinMax = ({min, max, value, onchange}) => {
             <input onInput={onCountChangeHandler} className="numeric-input__input" pattern="[0-9]+" value={current} />
             <button onClick={up} className="numeric-input__button numeric-input__button_up">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
-                    <path d="M6 9L8 7L10 9" stroke="#CACDD8" stroke-width="1.6" stroke-linecap="round"/>
+                    <path d="M6 9L8 7L10 9" stroke="#CACDD8" strokeWidth="1.6" strokeLinecap="round"/>
                 </svg>
             </button>
             <button onClick={down} className="numeric-input__button numeric-input__button_down">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
-                    <path d="M10 7L8 9L6 7" stroke="#CACDD8" stroke-width="1.6" stroke-linecap="round"/>
+                    <path d="M10 7L8 9L6 7" stroke="#CACDD8" strokeWidth="1.6" strokeLinecap="round"/>
                 </svg>
             </button>
         </span>
